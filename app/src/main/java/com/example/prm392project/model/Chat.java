f3b13e4b9214b4ebe2a8131fde5a4a5ec731d0b1
maura2112 <championlc97@gmail.com>
2023-11-05 23:54:18 +0700

    update Chat, Map

--- app/src/main/java/com/example/prm392project/model/Chat.java
+++ app/src/main/java/com/example/prm392project/model/Chat.java
@@ -1,28 +1,18 @@
 package com.example.prm392project.model;
 
 public class Chat {
-    private int id;
+
     private String msg;
 
     public Chat() {
     }
 
-    public Chat(int id, String msg) {
-        this.id = id;
-        this.msg = msg;
-    }
+
 
     public Chat(String msg) {
         this.msg = msg;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getMsg() {
         return msg;
--- app/src/main/java/com/example/prm392project/presentation/store/chat/MessageAdapter.java
+++ app/src/main/java/com/example/prm392project/presentation/store/chat/MessageAdapter.java
@@ -1,21 +1,45 @@
 package com.example.prm392project.presentation.store.chat;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MessageAdapter extends RecyclerView.Adapter {
+
+import com.example.prm392project.R;
+import com.example.prm392project.model.Chat;
+
+import java.util.List;
+
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.senderViewHolder> {
+
+    Context context;
+    List<Chat> messageList;
+
+
+    public MessageAdapter(Context context, List<Chat> messageList) {
+        this.context = context;
+        this.messageList = messageList;
+        notifyDataSetChanged();
+    }
+
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public senderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_chat, parent, false);
+        return new senderViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull senderViewHolder holder, int position) {
+        final Chat mes = messageList.get(position);
+        holder.msgtxt.setText(mes.getMsg());
     }
 
     @Override
@@ -23,17 +47,25 @@ public class MessageAdapter extends RecyclerView.Adapter {
         return 0;
     }
 
-    class senderViewHolder extends  RecyclerView.ViewHolder{
 
+    public class senderViewHolder extends RecyclerView.ViewHolder {
+        TextView msgtxt;
         public senderViewHolder(@NonNull View itemView) {
             super(itemView);
+            msgtxt = itemView.findViewById(R.id.msgsendertyp);
+
         }
     }
 
-    class reciverViewHolder extends  RecyclerView.ViewHolder{
+//    public class reciverViewHolder extends RecyclerView.ViewHolder {
+//        TextView msgtxt;
+//
+//        public reciverViewHolder(@NonNull View itemView) {
+//
+//            super(itemView);
+//            msgtxt = itemView.findViewById(R.id.recivertextset);
+//        }
+//    }
 
-        public reciverViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
-    }
+
 }
