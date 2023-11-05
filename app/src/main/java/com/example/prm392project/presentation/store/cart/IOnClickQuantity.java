3dffbf18abee018fa6f13b575aa232b243818e36
mailinh312 <maiphuonglinh312@gmail.com>
2023-11-06 01:32:47 +0700

    update cart: minus button, bonus button, total bill

---
+++ app/src/main/java/com/example/prm392project/presentation/store/cart/IOnClickQuantity.java
@@ -0,0 +1,9 @@
+package com.example.prm392project.presentation.store.cart;
+
+import com.example.prm392project.model.ItemCart;
+
+import java.util.List;
+
+public interface IOnClickQuantity {
+    void onClickListener();
+}
